b4e91ec5bded19f07b513b9a2b4cef8064436bb5
kh-Yu <ykhykh3587@naver.com>
2020-10-18 19:14:56 +0900

    Week7

---
+++ Week_7/New Unity Project/Assets/Scripts/InteractBtn.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractBtn : MonoBehaviour
+{
+    //플레이어 게임오브젝트
+    public GameObject player;
+    //상호작용 게임오브젝트의 갯수
+    public const int numCoins = 3;
+    public const int numHearts = 3;
+    //상호작용 게임오브젝트 배열 생성
+    public GameObject[] coins = new GameObject[numCoins];
+    public GameObject[] hearts = new GameObject[numHearts];
+    //플레이어의 position 객체 생성
+    Vector3 posPlayer;
+    //상호작용 position 배열 생성
+    Vector3[] posCoins = new Vector3[numCoins];
+    Vector3[] posHearts = new Vector3[numHearts];
+    //플레이어와 상호작용 오브젝트 사이의 거리 배열
+    private float[] btwCoins = new float[numCoins];
+    private float[] btwHearts = new float[numHearts];
+
+    Renderer objectColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        showDistance();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        posPlayer = player.gameObject.transform.position;
+
+        for (int i = 0; i < coins.Length; i++)
+        {
+            float distance = calcDistance(posPlayer, posCoins[i]);
+            if (distance > 4.0)
+            {
+                SpriteOutline objectBorder = coins[i].GetComponent<SpriteOutline>();
+                objectColor = coins[i].gameObject.GetComponent<Renderer>();
+                objectColor.material.color = Color.white;
+                objectBorder.enabled = false;
+            }
+            else if (distance <= 4.0 && distance > 2.0)
+            {
+                SpriteOutline objectBorder = coins[i].GetComponent<SpriteOutline>();
+                objectColor = coins[i].gameObject.GetComponent<Renderer>();
+                objectColor.material.color = Color.white;
+                objectBorder.enabled = true;
+            }
+            else
+            {
+                SpriteOutline objectBorder = coins[i].GetComponent<SpriteOutline>();
+                objectColor = coins[i].gameObject.GetComponent<Renderer>();
+                objectColor.material.color = new Color(255,255,0);
+                objectBorder.enabled = true;
+            }
+        }
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            float distance = calcDistance(posPlayer, posHearts[i]);
+            if (distance > 4.0)
+            {
+                SpriteOutline objectBorder = hearts[i].GetComponent<SpriteOutline>();
+                objectColor = hearts[i].gameObject.GetComponent<Renderer>();
+                objectColor.material.color = Color.white;
+                objectBorder.enabled = false;
+            }
+            else if (distance <= 4.0 && distance > 2.0)
+            {
+                SpriteOutline objectBorder = hearts[i].GetComponent<SpriteOutline>();
+                objectColor = hearts[i].gameObject.GetComponent<Renderer>();
+                objectColor.material.color = Color.white;
+                objectBorder.enabled = true;
+            }
+            else
+            {
+                SpriteOutline objectBorder = hearts[i].GetComponent<SpriteOutline>();
+                objectColor = hearts[i].gameObject.GetComponent<Renderer>();
+                objectColor.material.color = new Color(255, 255, 0);
+                objectBorder.enabled = true;
+            }
+        }
+    }
+
+    public float calcDistance(Vector3 posPlayer, Vector3 posObject)
+    {
+        float btwX = (posPlayer.x - posObject.x) * (posPlayer.x - posObject.x);
+        float btwY = (posPlayer.y - posObject.y) * (posPlayer.y - posObject.y);
+        float distance = Mathf.Sqrt(btwX + btwY);
+        return distance;
+    }
+
+    public void showDistance()
+    {
+        posPlayer = player.gameObject.transform.position;
+
+        for (int i = 0; i < numCoins; i++)
+        {
+            posCoins[i] = coins[i].gameObject.transform.position;
+            btwCoins[i] = calcDistance(posPlayer, posCoins[i]);
+            //Debug.Log("Coin " + i + " 과의 거리는 " + btwCoins[i] + " 입니다.");
+        }
+        for (int i = 0; i < numHearts; i++)
+        {
+            posHearts[i] = hearts[i].gameObject.transform.position;
+            btwHearts[i] = calcDistance(posPlayer, posHearts[i]);
+            //Debug.Log("Heart " + i + " 과의 거리는 " + btwHearts[i] + " 입니다.");
+        }
+    }
+}
---
+++ Week_7/New Unity Project/Assets/Scripts/MonoBehaviours/PlayerControllerScript.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+    public Joystick joystick;   //조이스틱 스크립트
+    public float MoveSpeed = 3.0f;     //플레이어 이동속도
+
+    public GameObject Map_Player, Player;
+
+    Animator animator;
+
+    string animationState = "AnimationState";
+    Rigidbody2D rb2D;
+
+    private Vector3 _moveVector, Player_current_location, Player_before_location; //플레이어 이동벡터, 플레이어의 현재 위치
+    private Transform _transform;
+
+    enum CharStates
+    {
+        walkEast = 1,
+        walkSouth = 2,
+        walkWest = 3,
+        walkNorth = 4,
+        idleSouth = 5
+    }
+
+    void Start()
+    {
+        _transform = transform;      //Transform 캐싱
+        _moveVector = Vector3.zero;  //플레이어 이동벡터 초기화
+        animator = GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        //터치패드 입력 받기
+        HandleInput();
+    }
+
+    private void FixedUpdate()
+    {
+
+        Player_before_location = Player.transform.position;
+        //플레이어 이동
+        Move();
+        if (_moveVector.x > 0)
+        {
+            animator.SetInteger(animationState, (int)CharStates.walkEast);
+        }
+        else if (_moveVector.x < 0)
+        {
+            animator.SetInteger(animationState, (int)CharStates.walkWest);
+        }
+        else if (_moveVector.y > 0)
+        {
+            animator.SetInteger(animationState, (int)CharStates.walkNorth);
+        }
+        else if (_moveVector.y < 0)
+        {
+            animator.SetInteger(animationState, (int)CharStates.walkSouth);
+        }
+        else
+        {
+            animator.SetInteger(animationState, (int)CharStates.idleSouth);
+        }
+
+    }
+
+    public void HandleInput()
+    {
+        Player_current_location = Player.transform.position;
+
+        _moveVector = PoolInput();
+        //Vector3 current_pos = Player.transform.position;
+        //Debug.Log(current_pos);
+        //Debug.Log(_moveVector);
+        Debug.Log("현재의 위치 : " + Player_current_location);
+
+        if (_moveVector.x + Player_current_location.x == Player_current_location.x)
+        {
+            Map_Player.transform.Translate(0, _moveVector.y / 900, 0);
+        }
+        else if (Player_current_location.y < -4.45f)
+        {
+            Map_Player.transform.Translate(0, 0, 0);
+        }
+        else if (Player_before_location.y == Player_current_location.y && Player_before_location.x == Player_current_location.x)
+        {
+            Debug.Log("멈춰라");
+            Map_Player.transform.Translate(0, 0, 0);
+        }
+        else
+        {
+            Map_Player.transform.Translate(_moveVector.x / 1200, _moveVector.y / 900, 0);
+        }
+    }
+
+    public Vector3 PoolInput()
+    {
+        float h = joystick.GetHorizontalValue();
+        float v = joystick.GetVerticalValue();
+        Vector3 moveDir = new Vector3(h, v, 0).normalized;
+
+        return moveDir;
+    }
+
+    public void Move()
+    {
+        _transform.Translate(_moveVector * MoveSpeed * Time.deltaTime);
+    }
+}
